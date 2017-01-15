d577ed9d9220c2ef66ebb0f48f017460530d5acf
Shaunak Sen <shaunak1105@gmail.com>
2017-01-15 16:31:55 +0530

    error handling + use of GET and POST vars in express

--- week-1/app3.js
+++ week-1/app3.js
@@ -2,6 +2,9 @@
 
 var express = require('express');
 var engines = require('consolidate');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -15,19 +18,72 @@ app.set("view engine", "html");
 
 // location of templates is specified
 app.set("views", __dirname + "/views");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Handler for internal server errors
+
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500);
+    res.render("error_template", {error: err});
+}
 
 
-app.get("/", function (req, res) {
-    res.render("hello", {name: "Mini"});
+app.use(errorHandler);
+
+
+MongoClient.connect("mongodb://localhost:27017/video", function (err, db) {
+
+    assert.equal(err, null);
+    console.log("Successfully connected to mongo :)");
+
+    app.get("/", function (req, res) {
+        console.log("here");
+        db.collection('movies').find({}).toArray( function (err, docs) {
+            console.log("Received docs");
+            console.log(docs);
+            res.render("movies", {"movies": docs});
+        });
+    });
+
+
+    // Accessing GET vars by express
+    app.get("/names/:name", function(req, res){
+        var name = req.params.name;
+        // console.log("query vars: " + req.query);
+        var getVar1 = req.query.getvar1;
+        var getVar2 = req.query.getvar2;
+        res.render("hello", {name: name, getvar1: getVar1, getvar2: getVar2});
+    });
+
+    // fruits route
+
+    app.get("/fruits", function (req, res) {
+        res.render("fruitPicker", {"fruits": ["apple", "orange", "grapes", "guava", "peach"]});
+    });
+
+    app.post("/fruits", function (req, res, next) {
+        var favoriteFruit = req.body.fruit;
+        if(typeof favoriteFruit == "undefined"){
+            next(Error('Please chose a fruit'));
+            // express will try to handle error
+            // it will look for error handling middleware that we have registered
+        } else {
+            res.send("Your favorite fruit is " + favoriteFruit);
+        }
+    });
+
+    // routes not handled by our route handler
+    app.use(function (req, res) {
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function () {
+        var port = server.address().port;
+        console.log("Server listening on port %s", port);
+    });
 });
 
 
-// routes not handled by our route handler
-app.use(function (req, res) {
-    res.sendStatus(404);
-});
 
-var server = app.listen(3000, function () {
-    var port = server.address().port;
-    console.log("Server listening on port %s", port);
-});
